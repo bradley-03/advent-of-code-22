e8cbe2a0e0258931ad86a43a88f7e6b3fbe3670f
Bradley <bradleymelia2003@gmail.com>
2022-12-15 01:16:58 +0000

    day 5 puzzle 2

---
+++ Day 5/puzzle_2.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const input = fs.readFileSync('Day 5/input.txt', 'utf8').split('\n')
+
+const rows = []
+for (let i = 0; i < 8; i++) { // sort rows into column equivalent
+    const saltedString = input[i].replace(/(.{3})./g,"$1")
+    rows.push(saltedString.match(/.{1,3}/g))
+}
+
+const columns = []
+for (let i = 0; i < 9; i++) { // create the 9 columns
+    columns.push([])
+} 
+
+for (let row = 7; row >= 0; row--) { // loop for each row
+    for (let i = 8; i >= 0; i--) { // loop for each column
+        if (rows[row][i] !== '   ') {
+            columns[i].push(rows[row][i])
+        }
+    }
+}
+
+const instructions = input.splice(10) // separate instructions from grid
+
+for (let instruction of instructions) {
+    const sInst = instruction.split(" ")
+
+    const removedItems = columns[parseInt(sInst[3]) - 1].slice(columns[parseInt(sInst[3]) - 1].length - parseInt(sInst[1]))
+    for (let i = 0; i < removedItems.length; i++) {
+        columns[parseInt(sInst[3]) - 1].pop()
+    }
+    
+    if (removedItems) {
+        columns[parseInt(sInst[5]) - 1].push(...removedItems) // remove .reverse() for puzzle 2 solution
+    }
+}
+
+const topStacks = []
+for (let column of columns) {
+    topStacks.push(column[column.length - 1][1])
+}
+
+console.log(`puzzle 2: ${topStacks.join('')}`) // puzzle 2 output
